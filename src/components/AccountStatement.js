07e5355777d921444db754d3aba51f9494d551a3
rutaks <rootsum.dev@gmail.com>
2020-04-17 11:42:01 +0200

    Added Statement Difference Bar

--- src/components/AccountStatement.js
+++ src/components/AccountStatement.js
@@ -9,18 +9,45 @@ export default function AccountStatement() {
     .reduce((acc, currVal) => (acc += currVal), 0)
     .toFixed(2);
   let expense = amount
-    .filter((item) => item < 1)
+    .filter((item) => item < -1)
     .reduce((acc, currVal) => (acc += currVal), 0)
     .toFixed(2);
+  const totalAmount = Math.abs(income) + Math.abs(expense);
+  let incomePercent = (100 * income) / totalAmount;
+  let expensePercent = (100 * Math.abs(expense)) / totalAmount;
+  console.log(totalAmount);
+  // console.log(Math.abs(amount));
+  // console.log(Math.abs(amount));
+
   return (
-    <div className="inc-exp-container">
-      <div>
-        <h6>Income</h6>
-        <p className="money plus">{income}</p>
+    <div>
+      <div className="inc-exp-container">
+        <div>
+          <h6>Income</h6>
+          <p className="money plus">{income}</p>
+        </div>
+        <div>
+          <h6>Expense</h6>
+          <p className="money minus">{expense}</p>
+        </div>
       </div>
-      <div>
-        <h6>Expense</h6>
-        <p className="money minus">{expense}</p>
+      <div class="progress">
+        <div
+          class="progress-bar bg-success progress-bar-striped progress-bar-animated"
+          role="progressbar"
+          style={{ width: `${incomePercent}%` }}
+          aria-valuenow="30"
+          aria-valuemin="0"
+          aria-valuemax="100"
+        ></div>
+        <div
+          class="progress-bar bg-danger progress-bar-striped progress-bar-animated"
+          role="progressbar"
+          style={{ width: `${expensePercent}%` }}
+          aria-valuenow="20"
+          aria-valuemin="0"
+          aria-valuemax="100"
+        ></div>
       </div>
     </div>
   );
